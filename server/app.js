b0869212ca353941df4decb18473f9d6f7bf6c57
Yuke <ray.b.hinchliffe@gmail.com>
2019-05-14 11:16:46 -0700

    boilerplate update

--- server/app.js
+++ server/app.js
@@ -2,9 +2,15 @@ const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
 
-mongoose.connect("mongodb://localhost/stocker", { useNewUrlParser: true })
-
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+
+app.use(require('./routes'))
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"))
+  }
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/stocker", { useNewUrlParser: true })
 
 module.exports = app
